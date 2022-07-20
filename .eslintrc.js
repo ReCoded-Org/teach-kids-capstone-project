62993465b42ad80890d4defe287ffb990987cac0
hiba <hiba.mashfij@gmail.com>
2022-07-20 15:54:58 +0300

    refactor: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,39 +5,24 @@ module.exports = {
         node: true,
         jest: true,
     },
-    plugins: ["react", "simple-import-sort", "unused-imports"],
     extends: [
-        "eslint:recommended",
+        "react-app",
+        "react-app/jest",
         "plugin:react/recommended",
-        "plugin:prettier/recommended",
+        "prettier",
     ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "prettier"],
     rules: {
-        "react/react-in-jsx-scope": "off",
         "no-unused-vars": "off",
         "no-console": "warn",
-        "react/jsx-curly-spacing": [1, "always"],
         "react/display-name": "off",
-        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-        //#region  //*=========== Unused Import ===========
-        "unused-imports/no-unused-imports": "warn",
-        "unused-imports/no-unused-vars": [
-            "warn",
-            {
-                vars: "all",
-                varsIgnorePattern: "^_",
-                args: "after-used",
-                argsIgnorePattern: "^_",
-            },
-        ],
-        //#endregion  //*======== Unused Import ===========
-
-        //#region  //*=========== Import Sort ===========
-        "simple-import-sort/exports": "warn",
-        "simple-import-sort/imports": ["warn", {}],
-        //#endregion  //*======== Import Sort ===========
-    },
-    globals: {
-        React: true,
-        JSX: true,
+        "react/react-in-jsx-scope": "off",
     },
 };
