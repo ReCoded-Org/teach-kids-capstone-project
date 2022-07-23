d8a4d2094494b603f491898738fbf2c89628d85c
mustafa Rast <mularas78@gmail.com>
2022-07-23 14:15:17 +0300

    test: Adding Navbar test

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../components/layout/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import "../App.css";
 
 // colors{1D3557,F05454}
