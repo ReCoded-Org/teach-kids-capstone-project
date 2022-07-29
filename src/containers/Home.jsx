77da8c1a38c63071323fba27af504c801b3d231d
R_Mustafa <101436017+RastMustafa@users.noreply.github.com>
2022-07-29 21:36:22 +0300

    Update Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Navbar from "../components/layout/Navbar/Navbar";
 import "../App.css";
 import HeroSection from "../components/HeroSection/HeroSection";
-import Events from "../components/GlobalCarousel/Events/Events";
-import Event from "../components/EventCarousel/Events/Events";
+import Events from "../components/EventCarousel/Events/Events";
 function Home() {
     return (
         <>
-            {/* <Navbar />
-            <HeroSection /> */}
-            <Events carouselHeader='Hello WOrld' />
+             <Navbar />
+            <HeroSection /> 
             <Event />
         </>
     );
