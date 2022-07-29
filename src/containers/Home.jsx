9c03fd27b989d71db571b334864f3a2699f371d8
mustafa Rast <mularas78@gmail.com>
2022-07-29 21:33:59 +0300

    naming convintions

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import Navbar from "../components/layout/Navbar/Navbar";
 import "../App.css";
 import HeroSection from "../components/HeroSection/HeroSection";
-import Events from "../components/GlobalCarousel/Events/Events";
-import Event from "../components/EventCarousel/Events/Events";
+import Events from "../components/EventCarousel/Events/Events";
 function Home() {
     return (
         <>
-            {/* <Navbar />
-            <HeroSection /> */}
-            <Events carouselHeader='Hello WOrld' />
-            <Event />
+            <Navbar />
+            <HeroSection />
+            <Events />
         </>
     );
 }
