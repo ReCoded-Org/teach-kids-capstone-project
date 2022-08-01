d4cf489fd1de4b30a3254dbd13805667c6b3ff9e
mustafa Rast <mularas78@gmail.com>
2022-08-01 20:40:55 +0300

    feat: Adding single event page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Home from "./containers/Home";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import SingleEventPage from "./containers/SingleEventPage";
 function App() {
     return (
         <div>
             <Routes>
                 {/* <Route path='/events' element={<Events />} /> */}
-                {/* <Route path='/event:id' element={<Event />} /> */}
+                {/* <Route path='/event:id' element={<SingleEventPage />} /> */}
                 {/* <Route path='/sign-in' element={<SignIn />} /> */}
                 {/* <Route path='/sign-up' element={<VolunteerOrNgo />} /> */}
                 {/* <Route path='/volunteer-profile' element={< VolunteerProfile. />} /> */}
                 {/* <Route path='/ngo' element={<NGO />} /> */}
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<SingleEventPage />} />
             </Routes>
         </div>
     );
---
+++ src/containers/SingleEventPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SingleEventPageHeroSection from "../components/SingleEventPageHeroSection/SingleEventPageHeroSection";
+import Navbar from "../components/layout/Navbar/Navbar";
+
+function SingleEventPage() {
+    return (
+        <>
+            <Navbar />
+            <SingleEventPageHeroSection />
+        </>
+    );
+}
+
+export default SingleEventPage;
