d5e8e9ac7184877640cb728772d7d462ab083580
Kinan Hatahet <kinan.hatahet@gmail.com>
2022-08-05 09:31:44 +0300

    refactor: removed the previously created 2 JSON files and added the data on db.json instead

--- src/components/EventsGrid/FilterEvents/FilterEvents.jsx
+++ src/components/EventsGrid/FilterEvents/FilterEvents.jsx
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./FilterEvents.css";
 
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useRef } from "react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+
 
 export default function FilterEvents() {
+  const [events, setEvents] = useState({});
     return (
         <>
             <div className='absolute w-full bg-gray pb-12'>
@@ -30,7 +36,6 @@ export default function FilterEvents() {
                             >
                                 <path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z' />
                             </svg>
-                            
                         </button>
 
                         <button
@@ -49,6 +54,52 @@ export default function FilterEvents() {
                                 <path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z' />
                             </svg>
                         </button>
+
+                        <div className='fixed top-16 w-56 text-right'>
+                            <Menu
+                                as='div'
+                                className='relative inline-block text-left'
+                            >
+                                <div>
+                                    <Menu.Button className='button mw-40 inline-flex w-40 justify-center rounded-md bg-blue-light px-4 py-2 text-sm font-medium text-white hover:bg-opacity-70 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+                                        Topic
+                                        <ChevronDownIcon
+                                            className='ml-2 -mr-1 h-5 w-5 text-white hover:text-violet-100'
+                                            aria-hidden='true'
+                                        />
+                                    </Menu.Button>
+                                </div>
+                                <Transition
+                                    as={Fragment}
+                                    enter='transition ease-out duration-100'
+                                    enterFrom='transform opacity-0 scale-95'
+                                    enterTo='transform opacity-100 scale-100'
+                                    leave='transition ease-in duration-75'
+                                    leaveFrom='transform opacity-100 scale-100'
+                                    leaveTo='transform opacity-0 scale-95'
+                                >
+                                    <Menu.Items className='divide-gray-100 absolute right-0 mt-2 w-56 origin-top-right divide-y rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                        <div className='px-1 py-1 '>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                            active
+                                                                ? "bg-blue-light text-white"
+                                                                : "text-gray-900"
+                                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                    >
+                                                        {/* Edit */}
+                                                        events.map()
+
+                                                    </button>
+                                                )}
+                                            </Menu.Item>
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </Menu>
+                        </div>
                     </div>
                 </div>
             </section>
