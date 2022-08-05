9c5ddc9421a9d971d45b7a6ca63327546139fe64
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-08-05 15:44:00 +0300

    feat: adding Functionality to the contact us using json-server

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider} from 'react-query';
 import { BrowserRouter } from "react-router-dom";
-
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </QueryClientProvider>
     </React.StrictMode>
 );
 
