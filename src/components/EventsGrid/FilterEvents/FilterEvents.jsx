a3bd245915b04100046c37d7da2b4fecd44ec2b5
Kinan Hatahet <kinan.hatahet@gmail.com>
2022-08-09 14:10:50 +0300

    style: fixed some extra errors

--- src/components/EventsGrid/FilterEvents/FilterEvents.jsx
+++ src/components/EventsGrid/FilterEvents/FilterEvents.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./FilterEvents.css";
 
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useRef } from "react";
