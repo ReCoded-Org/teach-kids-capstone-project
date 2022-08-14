1abe4329859a8ece6555c62da1b645c9bcdabca0
hiba <hiba.mashfij@gmail.com>
2022-08-14 14:15:17 +0300

    refactor: update react-query

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,10 +1,9 @@
-import {React, useState }from "react";
+import { React, useState } from "react";
 import Phone from "../../assets/Phone.png";
 import Message from "../../assets/Message.png";
 import Location from "../../assets/Location.png";
 import "./ContactUs.css";
-import {useMutation } from 'react-query';
-
+import { useMutation } from "@tanstack/react-query";
 
 function ContactUs() {
     const [formData, setFormData] = useState({
@@ -12,95 +11,193 @@ function ContactUs() {
         email: "",
         Message: "",
     });
- 
+
     function handleChange(event) {
         const name = event.target.name;
         let value = event.target.value;
- 
+
         setFormData({ ...formData, [name]: value });
     }
- 
+
     function handleSubmit(event) {
         event.preventDefault();
     }
-    const SendtoContactUs=useMutation((ContactUsData)=>{
-        return fetch("http://localhost:3004/contactus",{
+    const SendtoContactUs = useMutation((ContactUsData) => {
+        return fetch("http://localhost:3004/contactus", {
             method: "POST",
-            headers:{
-                Accept:"application/json",
-                "content-Type":"application/json"
+            headers: {
+                Accept: "application/json",
+                "content-Type": "application/json",
             },
-            body:JSON.stringify(ContactUsData),
-        })
+            body: JSON.stringify(ContactUsData),
+        });
     });
     return (
-     <div className='ContactUs flex items-center justify-between'>
-      <div className="container mx-auto pt-16">
-      <div className="lg:flex ">
-          <div className="xl:w-4/5 lg:w-4/5 py-20">
-          <div className="col-md-6 col-sm-12">
-            <div className="contactInfo col">
-            <h1 className="text-6xl mb-10 " style={{color:"#1D3557",fontWeight: 'bold'}}>Contact Us</h1>
-              <p className="text-1xl mb-10 md:max-w-lg" style={{color:"#1D3557"}}>Get the understanding you need to channel the books of spells you have always been smitten with, and boost sales which cartoon rabbits love to steal! 
-              Get the understanding you need to</p>
-                <div className="flex">
-                  <div className="flex-initial w-14 grow h-5">
-                    <img src={Location} alt="Location icon" />
-                    <p style={{color:"#1D3557"}}>9094 Bay Meadows Street Conyers, GA 30012</p>
-                    </div>
-                    <div className="flex-initial w-14 grow h-5">
-                      <img src={Phone} alt="Phone icon" />
-                      <p style={{color:"#1D3557"}}>+229-955-5388-336</p>
-                      <p style={{color:"#1D3557"}}>+229-955-5373-360</p>
-                      </div>
-                      <div className="flex-initial w-14 grow h-5">
-                        <img src={Message} alt="Contact icon" />
-                        <p style={{color:"#1D3557"}}>support@mail.com</p>
-                        <p style={{color:"#1D3557"}}>office@mail.com</p>
+        <div className='ContactUs flex items-center justify-between'>
+            <div className='container mx-auto pt-16'>
+                <div className='lg:flex '>
+                    <div className='py-20 lg:w-4/5 xl:w-4/5'>
+                        <div className='col-md-6 col-sm-12'>
+                            <div className='contactInfo col'>
+                                <h1
+                                    className='mb-10 text-6xl '
+                                    style={{
+                                        color: "#1D3557",
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    Contact Us
+                                </h1>
+                                <p
+                                    className='text-1xl mb-10 md:max-w-lg'
+                                    style={{ color: "#1D3557" }}
+                                >
+                                    Get the understanding you need to channel
+                                    the books of spells you have always been
+                                    smitten with, and boost sales which cartoon
+                                    rabbits love to steal! Get the understanding
+                                    you need to
+                                </p>
+                                <div className='flex'>
+                                    <div className='h-5 w-14 flex-initial grow'>
+                                        <img
+                                            src={Location}
+                                            alt='Location icon'
+                                        />
+                                        <p style={{ color: "#1D3557" }}>
+                                            9094 Bay Meadows Street Conyers, GA
+                                            30012
+                                        </p>
+                                    </div>
+                                    <div className='h-5 w-14 flex-initial grow'>
+                                        <img src={Phone} alt='Phone icon' />
+                                        <p style={{ color: "#1D3557" }}>
+                                            +229-955-5388-336
+                                        </p>
+                                        <p style={{ color: "#1D3557" }}>
+                                            +229-955-5373-360
+                                        </p>
+                                    </div>
+                                    <div className='h-5 w-14 flex-initial grow'>
+                                        <img src={Message} alt='Contact icon' />
+                                        <p style={{ color: "#1D3557" }}>
+                                            support@mail.com
+                                        </p>
+                                        <p style={{ color: "#1D3557" }}>
+                                            office@mail.com
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                      </div>
-                      </div>
-          </div>
-          <div className="FormContactUs xl:w-3/5 lg:w-3/5 pt-5 pb-5 xl:pr-5 xl:pl-0 rounded-tr">
-              <form id="contact" className="bg-white dark:bg-gray-800 py-4 px-8 rounded-tr" onSubmit={handleSubmit}>
-                  <div className="block xl:flex w-full flex-wrap justify-between mb-6">
-                      <div className="w-2/4 max-w-xs mb-6 xl:mb-0">
-                          <div className="flex flex-col">
-                              <label className="text-sm font-semibold mb-2" style={{color:"#1D3557"}}>Full Name</label>
-                              <input required id="Fullname" name="Fullname" type="text" onChange={handleChange}value={formData.Fullname} className="FormTextContactUs focus:border focus:border-indigo-700 h-10 rounded border" style={{backgroundColor:"#DDDDDD",borderWidth: 0,width:"580px"}} placeholder="Full Name"/>
-                          </div>
-                      </div>
-                      
-                  </div>
-                  <div className="flex w-full flex-wrap">
-                      <div className="w-2/4 max-w-xs">
-                          <div className="flex flex-col">
-                          <label className="text-sm font-semibold mb-2" style={{color:"#1D3557"}}>Email</label>
-                              <input required id="email" name="email" type="email"   onChange={handleChange}value={formData.email} className="FormTextContactUs focus:border focus:border-indigo-700 h-10 rounded border"style={{backgroundColor:"#DDDDDD",borderWidth: 0 ,width:"580px"}} placeholder="example@email.com"/>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="w-full mt-6">
-                      <div className="flex flex-col">
-                      <label className="text-sm font-semibold mb-2" style={{color:"#1D3557"}}>Message</label>
-                          <textarea placeholder="Type Here" name="Message" className="FormTextContactUs border-gray-300 border mb-4 rounded" onChange={handleChange}value={formData.Message} rows="9" id="Message" style={{backgroundColor:"#DDDDDD",borderWidth: 0,width:"580px"}} />
-                      </div>
-                      <button type="submit" className="rounded text-white px-8 py-3 focus:border-4 w-64" style={{backgroundColor:"#1D3557",borderWidth: 0}}onClick={() => {
-                    SendtoContactUs.mutate({
-                        Fullname:formData.Fullname,
-                        email:formData.email,
-                        Message:formData.Message,
-                    });
-                }}
-                >Send</button>
-                  </div>
-              </form>
-          </div>
-      </div>
-      </div>
-  </div>
-    )
+                    </div>
+                    <div className='FormContactUs rounded-tr pt-5 pb-5 lg:w-3/5 xl:w-3/5 xl:pr-5 xl:pl-0'>
+                        <form
+                            id='contact'
+                            className='dark:bg-gray-800 rounded-tr bg-white py-4 px-8'
+                            onSubmit={handleSubmit}
+                        >
+                            <div className='mb-6 block w-full flex-wrap justify-between xl:flex'>
+                                <div className='mb-6 w-2/4 max-w-xs xl:mb-0'>
+                                    <div className='flex flex-col'>
+                                        <label
+                                            className='mb-2 text-sm font-semibold'
+                                            style={{ color: "#1D3557" }}
+                                        >
+                                            Full Name
+                                        </label>
+                                        <input
+                                            required
+                                            id='Fullname'
+                                            name='Fullname'
+                                            type='text'
+                                            onChange={handleChange}
+                                            value={formData.Fullname}
+                                            className='FormTextContactUs h-10 rounded border focus:border focus:border-indigo-700'
+                                            style={{
+                                                backgroundColor: "#DDDDDD",
+                                                borderWidth: 0,
+                                                width: "580px",
+                                            }}
+                                            placeholder='Full Name'
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <div className='w-2/4 max-w-xs'>
+                                    <div className='flex flex-col'>
+                                        <label
+                                            className='mb-2 text-sm font-semibold'
+                                            style={{ color: "#1D3557" }}
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            required
+                                            id='email'
+                                            name='email'
+                                            type='email'
+                                            onChange={handleChange}
+                                            value={formData.email}
+                                            className='FormTextContactUs h-10 rounded border focus:border focus:border-indigo-700'
+                                            style={{
+                                                backgroundColor: "#DDDDDD",
+                                                borderWidth: 0,
+                                                width: "580px",
+                                            }}
+                                            placeholder='example@email.com'
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='mt-6 w-full'>
+                                <div className='flex flex-col'>
+                                    <label
+                                        className='mb-2 text-sm font-semibold'
+                                        style={{ color: "#1D3557" }}
+                                    >
+                                        Message
+                                    </label>
+                                    <textarea
+                                        placeholder='Type Here'
+                                        name='Message'
+                                        className='FormTextContactUs border-gray-300 mb-4 rounded border'
+                                        onChange={handleChange}
+                                        value={formData.Message}
+                                        rows='9'
+                                        id='Message'
+                                        style={{
+                                            backgroundColor: "#DDDDDD",
+                                            borderWidth: 0,
+                                            width: "580px",
+                                        }}
+                                    />
+                                </div>
+                                <button
+                                    type='submit'
+                                    className='w-64 rounded px-8 py-3 text-white focus:border-4'
+                                    style={{
+                                        backgroundColor: "#1D3557",
+                                        borderWidth: 0,
+                                    }}
+                                    onClick={() => {
+                                        SendtoContactUs.mutate({
+                                            Fullname: formData.Fullname,
+                                            email: formData.email,
+                                            Message: formData.Message,
+                                        });
+                                    }}
+                                >
+                                    Send
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ContactUs;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider} from 'react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </QueryClientProvider>
     </React.StrictMode>
 );
