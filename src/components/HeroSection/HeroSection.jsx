6412eda5e45bb02eba19ba54a56d757712df26f2
Kinan Hatahet <kinan.hatahet@gmail.com>
2022-08-20 13:39:40 +0300

    refactor: finished implementing Arabic translation

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,25 +3,25 @@ import "../../App.css";
 import logo from "../../assets/landing_page 1.png";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
- 
+
 function HeroSection() {
     const [t] = useTranslation();
- 
+
     return (
         <>
-            <section className='my-3 grid grid-cols-1 bg-white p-4 py-8 sm:grid-cols-2 md:px-12 md:pt-0 md:my-4 lg:px-36 lg:py-28'>
+            <section className='my-3 grid grid-cols-1 bg-white p-4 py-8 sm:grid-cols-2 md:my-4 md:px-12 md:pt-0 lg:px-36 lg:py-28'>
                 <main className='row md:mt-20 lg:mt-20'>
-                    <h1 className='text-center my-4 mb-6  font-quicksand text-5xl font-semibold text-blue-dark md:text-start'>
-                        Reach
+                    <h1 className='my-4 mb-6 text-center  font-quicksand text-5xl font-semibold text-blue-dark md:text-start'>
+                        {t("home.hero.title")}
                     </h1>
-                    <p className='text-center text-xl my-6 mb-4 font-SourceSansPro leading-5 text-blue-dark md:text-start'>
+                    <p className='my-6 mb-4 text-center font-SourceSansPro text-xl leading-5 text-blue-dark md:text-start'>
                         {t("home.hero.description")}
                     </p>
                     <div className='h-18 my-3 flex items-center justify-center gap-2 py-2 text-center font-SourceSansPro  text-sm  sm:justify-start'>
-                    <Link to='/volunteer-profile'>
+                        <Link to='/volunteer-profile'>
                             <button
                                 href=''
-                                className='mw-40 w-32 rounded-md border-2 border-blue-light bg-blue-light px-2 py-2 text-white duration-300 ease-linear font-semibold hover:border-blue-dark hover:bg-white  hover:text-blue-dark hover:shadow'
+                                className='mw-40 w-32 rounded-md border-2 border-blue-light bg-blue-light px-2 py-2 font-semibold text-white duration-300 ease-linear hover:border-blue-dark hover:bg-white  hover:text-blue-dark hover:shadow'
                             >
                                 {t("home.hero.btn1")}
                             </button>
@@ -29,7 +29,7 @@ function HeroSection() {
                         <Link to='/events'>
                             <button
                                 href=''
-                                className='w-32 rounded-md border-2 border-blue-light bg-white py-2 px-2 text-blue-light font-semibold duration-300 ease-linear hover:border-2 hover:bg-blue-light hover:text-white hover:shadow '
+                                className='w-32 rounded-md border-2 border-blue-light bg-white py-2 px-2 font-semibold text-blue-light duration-300 ease-linear hover:border-2 hover:bg-blue-light hover:text-white hover:shadow '
                             >
                                 {t("home.hero.btn2")}
                             </button>
@@ -44,4 +44,3 @@ function HeroSection() {
     );
 }
 export default HeroSection;
- 
