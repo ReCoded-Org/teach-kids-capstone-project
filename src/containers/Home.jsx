9ffd2065848def9cad87a49a4675dbe4fe71fa9d
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-08-28 14:02:03 +0300

    Changing in Style

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import "../App.css";
 import HeroSection from "../components/HeroSection/HeroSection";
 import Events from "../components/EventCarousel/Events/Events";
-import {useLocation} from 'react-router-dom';
 import ContactUs from "../components/ContactUs/ContactUs";
 import AboutUs from "../components/AboutUs/AboutUs";
 import OurValues from "../components/OurValues/OurValues";
