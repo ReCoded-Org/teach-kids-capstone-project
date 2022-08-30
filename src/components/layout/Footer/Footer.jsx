4f4721449be3ad3e265d85f6411f8ed887c09083
Kinan Hatahet <kinan.hatahet@gmail.com>
2022-08-30 15:09:46 +0300

    style: activated the links and adjust the style

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -8,6 +8,8 @@ import youtube from "../../../assets/youtube.svg";
 import PostRequestFunction from "./PostRequestFunction";
 import { isValidEmail } from "../../../services/ValidationFunctions";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 export default function Footer() {
     const [subscriptionData, setSubscriptionData] = useState("");
@@ -17,22 +19,30 @@ export default function Footer() {
         <div className='bg-primary'>
             <div
                 data-testid='footer'
-                className='jus mx-auto flex w-9/12 flex-wrap items-center justify-center bg-primary py-10  font-SourceSansPro text-xl text-white md:px-8  lg:justify-between '
+                className='mx-auto flex w-9/12 flex-wrap items-center justify-between bg-primary py-10  font-SourceSansPro text-xl text-white md:px-8  lg:justify-between '
             >
                 <div className='my-2'>
                     <img className='pb-2' src={logo} alt='Logo' />
-                    <h6 className='flex justify-start pt-2'>
-                        {t(`footer.pages.${[0]}`)}
-                    </h6>
-                    <h6 className='flex justify-start pt-2'>
-                        {t(`footer.pages.${[1]}`)}
-                    </h6>
-                    <h6 className='flex justify-start pt-2'>
-                        {t(`footer.pages.${[2]}`)}
-                    </h6>
-                    <h6 className='flex justify-start pt-2'>
-                        {t(`footer.pages.${[3]}`)}
-                    </h6>
+                    <Link to='/events'>
+                        <h6 className='flex justify-start pt-2'>
+                            {t(`footer.pages.${[0]}`)}
+                        </h6>
+                    </Link>
+                    <HashLink smooth to='/#contact-us'>
+                        <h6 className='flex justify-start pt-2'>
+                            {t(`footer.pages.${[1]}`)}
+                        </h6>
+                    </HashLink>
+                    <HashLink smooth to='/#about'>
+                        <h6 className='flex justify-start pt-2'>
+                            {t(`footer.pages.${[2]}`)}
+                        </h6>
+                    </HashLink>
+                    <Link to='/our-team'>
+                        <h6 className='flex justify-start pt-2'>
+                            {t(`footer.pages.${[3]}`)}
+                        </h6>
+                    </Link>
                     <div className='flex pt-8'>
                         <img className='pr-8' src={twitter} alt='twitter' />
                         <img className='pr-8' src={linkedin} alt='linkedin' />
@@ -41,8 +51,8 @@ export default function Footer() {
                         <img className='pr-8' src={youtube} alt='youtube' />
                     </div>
                 </div>
-                <div className='t-10 flex flex-col lg:mr-6'>
-                    <h3 className='mb-5 flex justify-start pt-10 text-lg'>
+                <div className='t-10 flex flex-col'>
+                    <h3 className='mb-5 flex justify-start pt-10 text-2xl'>
                         {t(`footer.newsletter.title`)}
                     </h3>
                     <div className='flex justify-between'>
@@ -77,23 +87,25 @@ export default function Footer() {
                     </form>
                 </div>
             </div>
-            <div className='flex-wrap justify-around bg-secondary pb-2 pt-1 text-white sm:flex'>
+            <div className="bg-secondary">
+            <div className='mx-auto flex w-9/12 flex-col flex-wrap justify-start  pb-2 pt-1 text-white md:flex md:flex-row md:justify-between'>
                 <div className='grid grid-cols-1'>
-                    <p className='my-1 justify-self-start pl-5 text-xs'>
+                    <p className='my-1 justify-self-start md:pl-5 text-xs'>
                         {t(`footer.lowerFooter.${[0]}`)}
                     </p>
                 </div>
                 <div className='grid grid-cols-1 sm:flex sm:justify-between sm:justify-items-stretch'>
-                    <div className='my-1 justify-self-start pl-5 text-xs'>
+                    <div className='my-1 justify-self-start md:pl-5 text-xs'>
                         {t(`footer.lowerFooter.${[1]}`)}
                     </div>
-                    <div className='my-1 justify-self-start pl-5 text-xs'>
+                    <div className='my-1 justify-self-start md:pl-5 text-xs'>
                         {t(`footer.lowerFooter.${[2]}`)}
                     </div>
-                    <div className='my-1 justify-self-start pl-5 pr-2 text-xs'>
+                    <div className='my-1 justify-self-start md:pl-5 pr-2 text-xs'>
                         {t(`footer.lowerFooter.${[3]}`)}
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     );
